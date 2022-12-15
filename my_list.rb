0997f9dbbaf6290e952cdd862756477803273622
cvalencia1991 <cesar4a6z@gmail.com>
2022-12-15 18:08:50 -0500

    Add Class Method

---
+++ my_list.rb
@@ -0,0 +1,20 @@
+require_relative 'enumerable'
+
+class Mylist
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+list = Mylist.new(1, 2, 3, 4)
+
+puts(list.all? { |i| i < 5 })
+puts(list.any? { |i| i > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts(list.filter(&:even?))
