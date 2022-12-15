917760a8247ae8734d922edd47ad0542105257d5
cvalencia1991 <cesar4a6z@gmail.com>
2022-12-15 18:27:36 -0500

    Change enumerable module

--- enumerable.rb
+++ enumerable.rb
@@ -1,26 +1,23 @@
-class Mylist
-  def initialize(value1,value2,value3, value4)
-   @list = [value1,value2,value3, value4]
+module MyEnumerable
+  def all?
+    list.each do |e|
+      return false unless yield e
+    end
+    true
   end
 
-  def MyEnumerable ()
-    list = @list
-    result = list.all? {|e| e < 5}
-    print result
+  def any?
+    list.each do |e|
+      return true if yield e
+    end
+    false
   end
 
+  def filter
+    result = []
+    list.each do |e|
+      result << e if yield e
+    end
+    result
+  end
 end
-
-list = Mylist.new(1,2,3,4)
-
-list.MyEnumerable()
-
-  # def MyEnumerable
-  #   # list = @list
-  #   # result = list.all? {|e| e < 5}
-  #   # result = list.all? {|e| e > 5}
-  #   # result = list.any? {|e| e == 2}
-  #   # result = list.any? {|e| e == 5}
-  #   result = list.filter {|e| e.even?}
-  #   print result
-  # end
