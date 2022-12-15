71e583a9a0f4fa721ea9f1d66be27c10b44080be
cvalencia1991 <cesar4a6z@gmail.com>
2022-12-15 16:02:00 -0500

    Add testing All

--- enumerable.rb
+++ enumerable.rb
@@ -1,4 +1,16 @@
 class Mylist
-  def Myenumerable
+  def initialize(value1,value2,value3, value4)
+   @list = [value1,value2,value3, value4]
   end
+
+  def MyEnumerable ()
+    list = @list
+    result = list.all? {|e| e < 5}
+    print result
+  end
+
 end
+
+list = Mylist.new(1,2,3,4)
+
+list.MyEnumerable()
